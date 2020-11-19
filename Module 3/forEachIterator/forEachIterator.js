442036b8848a9286a01fb0a3d534f7f48713e4a6
Ashwini_Bhirudharaju <Ashwini_Bhirudharaju@W10537JNQ2>
2020-11-19 20:21:19 +0800

    lwc

---
+++ Module 3/forEachIterator/forEachIterator.js
@@ -0,0 +1,12 @@
+import { LightningElement } from 'lwc';
+
+export default class ForEachIterator extends LightningElement {
+
+    contacts=[  
+        {id:'a1',Name:'aishwarya',Email:'aish@gmail.com'},
+        {id:'a2',Name:'siri',Email:'siri@gmail.com'},
+        {id:'a3',Name:'vishnu',Email:'vishnu@gmail.com'},
+        {id:'a4',Name:'sitara',Email:'sitara@marvelcomics.com'}
+
+    ];
+}
